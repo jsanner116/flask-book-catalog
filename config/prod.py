a180edf6d83214f7c37e6d010bd368c621569b87
jsanner116 <johnsanner116@gmail.com>
2021-10-15 13:49:35 -0700

    error fixes

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG = False
-SECRET_KEY='topsecret',
-SQLALCHEMY_DATABASE_URI = os.environ.get['DATABASE_URL'].replace("://", "ql://", 1)
-SQLALCHEMY_TRACK_MODIFICATIONS=False
+SECRET_KEY = 'topsecret',
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
