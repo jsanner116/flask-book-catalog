c66a94c1ee2b932b1aa881aea97d229b9bc49ba3
jsanner116 <johnsanner116@gmail.com>
2021-10-15 13:57:29 -0700

    error fixing

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,10 @@
 import os
+import re
 
 DEBUG = False
 SECRET_KEY = 'topsecret',
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
