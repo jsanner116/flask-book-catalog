c8e5f404a587db9f9977e314fc7a4ee2b90d814f
jsanner116 <johnsanner116@gmail.com>
2021-10-15 08:34:41 -0700

    Flask Advanced

--- config/dev.py
+++ config/dev.py
@@ -2,4 +2,3 @@ DEBUG = True
 SECRET_KEY='topsecret',
 SQLALCHEMY_DATABASE_URI='postgresql://postgres:nmfPDiRExAxR3D*h!N7*@localhost/catalog_db'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
-
--- config/prod.py
+++ config/prod.py
@@ -1,2 +1,5 @@
-
+DEBUG = False
+SECRET_KEY='topsecret',
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:nmfPDiRExAxR3D*h!N7*@localhost/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
 
