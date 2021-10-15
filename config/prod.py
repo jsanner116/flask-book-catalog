03e3e8d5c569a54dc50e439073024be051d57d89
jsanner116 <johnsanner116@gmail.com>
2021-10-15 13:44:47 -0700

    more changes to run.py

--- config/prod.py
+++ config/prod.py
@@ -2,6 +2,6 @@ import os
 
 DEBUG = False
 SECRET_KEY='topsecret',
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ.get['DATABASE_URL'].replace("://", "ql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 
